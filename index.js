c91f2ba566412647c47782138a58af358fa9a6f6
Joao Pedro <jp29.05.2006@gmail.com>
2023-10-12 09:59:54 -0300

    adicionando parametros na rota

--- index.js
+++ index.js
@@ -5,11 +5,19 @@ const app = express()
 
 const caminhoBase = path.join(__dirname, "templates")  
 
+app.get('/usuarios:id', (requisicao, resposta) => {
+    const id = requisicao.parans.id
+
+    console.log(`Acessando dados do usuário ${id}`)
+
+    resposta.sendFile(`${caminhoBase}/usuarios.html`)
+})
+
 app.get('/', (requisicao, resposta) => {
     resposta.sendFile(`${caminhoBase}/index.html`)
 })
 
-app.listen(3000, () => {
+app.listen(5500, () => {
     console.log("Servidor rodando na porta 3000!")
 })
 
